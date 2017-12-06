7082a6bf849bd4c54fd76f776d03b8ee0d802ec9
Aftermath-XTd <aftermaththenoufou>
2017-12-06 12:24:23 +0000

    update portfolio

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Head from "./components/Head";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
-// import Project from "./components/Project";
 import "./index.css";
 
 let  about = {
